d46ebe1343de26057d5f4a9f455469d021ba9f26
Rishen <rishenlap@gmail.com>
2025-03-07 21:52:07 +0545

    oops:

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const urlDatabase = {};
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +22,46 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.get('/api/shorturl/:short_url',(req,res)=>
+  {
+    const code = req.params.short_url;
+    console.log(code);
+    if (urlDatabase[code]){
+      res.redirect(urlDatabase[code]);
+    }
+    else{
+      res.json({"error":"invalid url"});
+    }
+  });
+app.use('/api/shorturl',(req,res,next)=>
+{
+ try{
+  const url = new URL(req.body.url);
+  if (url.protocol !== 'http:' && url.protocol !== 'https:'){
+    return res.json({"error": "invalid url"});
+  }
+  dns.lookup(url.hostname, (err, address, family) => {
+    if (err) {
+        return res.json({ error: 'invalid url' });  // If DNS lookup fails
+    }
+
+    // If everything is valid, proceed to the next middleware
+    next();
+});
+ }
+ catch(error)
+ {
+  return res.json({"error":"invalid url"});
+ }
+});
+app.post('/api/shorturl', (req,res)=>
+{
+  const number = 2;
+  res.json({"original_url": req.body.url, "short_url": number});
+  urlDatabase[number] = req.body.url;
+});
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
